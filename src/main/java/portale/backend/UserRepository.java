718bcf7326e7cfc066e8aea5f595dd2a0e542914
Manuel9593 <mparrarodriguez95@gmail.com>
2024-02-12 18:01:25 +0100

    got to create jdbc

--- src/main/java/portale/backend/UserRepository.java
+++ src/main/java/portale/backend/UserRepository.java
@@ -1,23 +1,34 @@
 package portale.backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UserRepository {
 	private Map<String, User> users = new HashMap<>();
 	private static UserRepository instance = null;
+	private Connection con = null;
 	
-	private UserRepository() {
-		// Empty
+	private UserRepository() throws SQLException {
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "");
 	}
 	
 	public static UserRepository getInstance() {
-		if(instance == null) {
-			instance = new UserRepository();
-			instance.add(new User("test", "password", "Mario", "Rossi"));
-			instance.add(new User("admin", "admin", "Dario", "Bronzo"));
+		if (instance == null) {
+			try {
+				instance = new UserRepository();
+			} catch (SQLException e) {
+				log.error("Errore di connessione al DB");
+			}
 		}
 		return instance;
 	}
@@ -27,6 +38,16 @@ public class UserRepository {
 	}
 	
 	public User get(String login) {
+		try {
+			PreparedStatement ps = con.prepareStatement("select * from users where login = ?");
+			ps.setString(1, login);
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next())
+				return null;
+			return new User(login, rs.getString("password"), rs.getString("nome"), rs.getString("cognome"));
+		} catch(SQLException sql) {
+			log.error("Errore all'esecuzione della query");
+		}
 		return users.get(login);
 	}
 	
